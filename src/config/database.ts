1fd7342cd922cdbe6a51e40ef809ea0dc64611f7
Samuel Souza <samuel_dacruz@hotmail.com>
2024-05-04 10:52:43 -0300

    Mongodb (#5)
    * fix: config before queries
    
    * fix: update database functions

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,4 +1,4 @@
-import { Collection, MongoClient, Document, Db } from "mongodb";
+import { ServerApiVersion, MongoClient, Db } from "mongodb";
 
 export class Database {
   private databaseName: string;
@@ -9,22 +9,42 @@ export class Database {
     this.databaseName = databaseName;
     this.connectionString = connectionString;
 
-    this.mongo = new MongoClient(connectionString);
+    this.mongo = new MongoClient(connectionString, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      },
+    });
   }
 
   getMongo = async (): Promise<MongoClient> => {
     if (!this.mongo) {
-      this.mongo = new MongoClient(this.connectionString);
+      this.mongo = new MongoClient(this.connectionString, {
+        serverApi: {
+          version: ServerApiVersion.v1,
+          strict: true,
+          deprecationErrors: true,
+        },
+      });
     }
     return this.mongo;
   };
 
   getDatabase = async (): Promise<Db> => {
     const _mongo = await this.getMongo();
-    return _mongo.db(this.databaseName);
+    await _mongo.connect();
+    const db = await _mongo.db(this.databaseName);
+    await db.command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+    return db;
   };
 
   closeConnection = async (): Promise<void> => {
-    (await this.getMongo()).close();
+    const db = await this.getMongo();
+    await db.close();
+    console.log("Connection closed!");
   };
 }
